ac408c9548b7fde251a19b1a0f74fa61128e2e4c
Joaquin Bomas <0328205@student.rocvantwente.nl>
2021-03-11 12:22:09 +0100

    veel geleerd

--- Simpele Rekenmachine/Form1.cs
+++ Simpele Rekenmachine/Form1.cs
@@ -19,47 +19,79 @@ namespace Simpele_Rekenmachine
 
         private void button1_Click(object sender, EventArgs e)
         {
-            decimal operand1 = 0;
-            decimal operand2 = 0;
-            string operant = "";
-            operand1 = Convert.ToDecimal(txtOperand1.Text);
-            operant = txtOperator.Text;
+            try
+            {
+                decimal operand1 = 0;
+                decimal operand2 = 0;
+                string operant = "";
+                operand1 = Convert.ToDecimal(txtOperand1.Text);
+                operant = txtOperator.Text;
 
-            operand2 = Convert.ToDecimal(txtOperand2.Text);
-          
-            Calculate(operand1, operant, operand2);
-            
+                operand2 = Convert.ToDecimal(txtOperand2.Text);
+                bool gelukt = Decimal.TryParse(txtOperand2.Text, out operand2);
+                if (gelukt == false)
+                {
+                    throw new OverflowException("Operand2 is ongeldig.");
+                }
+                else
+                {
+                    throw new Exception("algemene melding.");
+                }
+
+                //Calculate(operand1, operant, operand2);
+
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private decimal Calculate(decimal operand1, string operant, decimal operand2)
         {
-            decimal resultaat = 0;
 
-            if (operant == "*")
+            try
             {
-                resultaat = operand1 * operand2;
-                txtResult.Text = Convert.ToString(resultaat);
-            }
+                decimal resultaat = 0;
 
-            if (operant == "/")
+                if (operant == "*")
+                {
+                    resultaat = operand1 * operand2;
+                    txtResult.Text = Convert.ToString(resultaat);
+                }
+
+                if (operant == "/")
+                {
+                    resultaat = operand1 / operand2;
+                    txtResult.Text = Convert.ToString(resultaat);
+                }
+
+                if (operant == "-")
+                {
+                    resultaat = operand1 - operand2;
+                    txtResult.Text = Convert.ToString(resultaat);
+                }
+
+                if (operant == "+")
+                {
+                    resultaat = operand1 + operand2;
+                    txtResult.Text = Convert.ToString(resultaat);
+                }
+
+                return resultaat;
+
+            }
+            catch (Exception ex)
             {
-                resultaat = operand1 / operand2;
-                txtResult.Text = Convert.ToString(resultaat);
+                MessageBox.Show(ex.Message);
+                return 0;
             }
-
-            if (operant == "-")
-            {
-                resultaat = operand1 - operand2;
-                txtResult.Text = Convert.ToString(resultaat);
-            }
-
-            if (operant == "+")
-            {
-                resultaat = operand1 + operand2;
-                txtResult.Text = Convert.ToString(resultaat);
-            }
-
-            return resultaat;
+            
 
         }
 
